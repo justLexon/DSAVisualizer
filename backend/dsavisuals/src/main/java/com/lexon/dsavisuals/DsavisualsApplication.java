02e1a7eef998c2fbf9fb880a91d633b563d7b87a
Lexon Schexnider <otherslexon@gmail.com>
2025-09-06 18:09:15 -0500

    added Maven Spring dir

---
+++ backend/dsavisuals/src/main/java/com/lexon/dsavisuals/DsavisualsApplication.java
@@ -0,0 +1,13 @@
+package com.lexon.dsavisuals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsavisualsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsavisualsApplication.class, args);
+	}
+
+}
---
+++ backend/dsavisuals/src/test/java/com/lexon/dsavisuals/DsavisualsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexon.dsavisuals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsavisualsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
